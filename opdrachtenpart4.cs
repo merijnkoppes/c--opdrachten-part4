2daf0f9de40119a7f522829916db1715cf943ba6
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2023-03-15 09:58:13 +0100

    opdracht_4-3

--- opdrachtenpart4.cs
+++ opdrachtenpart4.cs
@@ -1,20 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Security.Cryptography.X509Certificates;
+﻿using System;
 
 public class Opdrachtenpart2
 {
- 
-
     public static void Main(string[] args)
     {
-       
+        Dog hond1 = new Dog("hond1", "ras", 5);
+        Console.WriteLine(hond1.Name);
+        Console.ReadKey();
     }
 
+    public class Dog
+    {
+        private string name;
+        private string breed;
+        private int age;
 
+        public Dog(string name, string breed, int age)
+        {
+            this.name = name;
+            this.breed = breed;
+            this.age = age;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
 
+        public string Breed
+        {
+            get { return breed; }
+            set { breed = value; }
+        }
 
-
-
-
-
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
 }
